159bdcc8c6ddee03392d19d783afa335830c1263
ltcxjtu <1440935236@qq.com>
2017-06-19 20:36:13 +0800

    2016-06-19

--- Localisation/beamformerFindMaxima.m
+++ Localisation/beamformerFindMaxima.m
@@ -18,5 +18,119 @@ function myBeamformer = beamformerFindMaxima ( myBeamformer , myPreprocessor )
 
     myBeamformer.myRij=rijProcess(myBeamformer.myRij);
 
+        /***************************************************************************
+    * Step 2: Find potential sources                                           *
+    ***************************************************************************/
+    for indexPotential=1:myBeamformer.BF_MAXSOURCES
+
+        // +-------------------------------------------------------------------+
+        // | Step A: Find the maximum                                          |
+        // +-------------------------------------------------------------------+
+
+        maxValue = -eps;
+        maxIndex = -1;
+
+        for indexPoint = 1:myBeamformer.myDelays.nPoints
+            sumn=0;
+            for indexPair=1:myBeamformer.myDelays.nPairs
+                delay = myBeamformer.myDelays.lookup(indexPoint,indexPair);
+                sumn=sumn+myBeamformer.myRij.crossCorrFiltered(indexpair,delay - myBeamformer.myRij.delayMin);
+            end
+
+            if (sumn>maxValue)
+                maxValue = sumn;
+                maxIndex = indexPoint;
+            end
+        end
+
+        myBeamformer.maxValue(indexPotential)= maxValue;
+        myBeamformer.maxIndex(indexPotential)=maxIndex;
+
+        // +-------------------------------------------------------------------+
+        // | Step B: Reset the delays associated with the maximum              |
+        // +-------------------------------------------------------------------+
+
+        // First verify that this was not the last iteration so that delays
+        // are reset only if needed
+
+        if (~(indexPotential == (myBeamformer.BF_MAXSOURCES - 1))
+            // Reset
+            rijRemoveSource(myBeamformer.myRij, myBeamformer.myDelays, maxIndex);
+        end
+
+
+
+
+    /***************************************************************************
+    * Step 3: Generate potential sources and probabilities                     *
+    ***************************************************************************/
+
+    %for (indexPotential = 0; indexPotential < myBeamformer->BF_MAXSOURCES; indexPotential++)
+    for indexPotential = 1:myBeamformer.BF_MAXSOURCES
+
+        // +-------------------------------------------------------------------+
+        // | Step A: Get positions                                             |
+        // +-------------------------------------------------------------------+
+
+        indexPoint = myBeamformer.maxIndexes(indexPotential);
+
+        x = sphereGetX(myBeamformer->mySphere, indexPoint);
+        y = sphereGetY(myBeamformer->mySphere, indexPoint);
+        z = sphereGetZ(myBeamformer->mySphere, indexPoint);
+
+        mySources.sourcesPosition[indexPotential][0] = x;
+        mySources.sourcesPosition[indexPotential][1] = y;
+        mySources.sourcesPosition[indexPotential][2] = z;
+
+        mySources.sourcesPosition(indexPotential,:) = myBeamformer.mySphere(indexPoint,:);
+
+        // +-------------------------------------------------------------------+
+        // | Step B: Get probability                                           |
+        // +-------------------------------------------------------------------+
+
+        energy = myBeamformer.maxValues(indexPotential);
+
+        switch (indexPotential)
+        {
+
+        case 0:
+
+            v = energy / myBeamformer->BF_ET;
+
+            if (v <= 1.0)
+            {
+                probability = v * v / 2.0;
+            }
+            else
+            {
+                probability = 1.0 - (1.0 / (v * v * 2.0));
+            }
+            break;
+
+        case 1:
+            probability = 0.3;
+            break;
+
+        case 2:
+            probability = 0.16;
+            break;
+
+        case 3:
+            probability = 0.03;
+            break;
+
+        default:
+            probability = 0.01;
+            break;
+
+        mySources.sourcesProbability(indexPotential) = probability;
+
+    }
+
+
+
+
+
+
 
 end
---
+++ Localisation/rijRemoveSource.m
@@ -0,0 +1,36 @@
+function  rijRemoveSource(myRij,myDelays,indexPoint)
+
+    for indexPair = 1:myRij.myMicrophones.nPairs
+
+    //for (indexPair = 0; indexPair < myRij->myMicrophones->nPairs; indexPair++)
+
+        // +-------------------------------------------------------------------+
+        // | Step A: Get delay for this pair and point                         |
+        // +-------------------------------------------------------------------+
+
+        delay = myDelays.lookup(indexPoint,indexPair);
+
+        // +-------------------------------------------------------------------+
+        // | Step B: Get the range that needs to be reset                      |
+        // +-------------------------------------------------------------------+
+
+        delayMin = delay - myRij.RIJ_RESETRANGE;
+
+        if (delayMin < myRij.delayMin)
+            delayMin = myRij.delayMin;
+        end
+
+        delayMax = delay + myRij.RIJ_RESETRANGE;
+        if (delayMax > myRij.delayMax)
+            delayMax = myRij.delayMax;
+        end
+
+        // +-------------------------------------------------------------------+
+        // | Step C: Reset                                                     |
+        // +-------------------------------------------------------------------+
+
+        for delay = delayMin:delayMax        
+            myRij.crossCorrFiltered(indexPair,delay - myRij.delayMin) = 0.0;
+        end
+
+end
